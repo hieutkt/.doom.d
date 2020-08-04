0869ac47336dd58c47484d9a91ceb6da7cde750c
hieutkt <hieunguyen31371@gmail.com>
2020-08-04 17:21:32 +0700

    Make evil-escape with `jk` a bit more responsive

--- config.el
+++ config.el
@@ -60,6 +60,9 @@
 ;;
 ;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
 ;; they are implemented.
+(use-package! evil-escape
+  :config
+  (setq evil-esc-delay 0.25))
 
 
 (use-package! org

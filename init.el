4070ada4628ed61824a381b2406902cf6a3b337f
hieutkt <hieunguyen31371@gmail.com>
2020-08-04 18:11:55 +0700

    Add `biblio` and `yaml` modules

--- init.el
+++ init.el
@@ -83,6 +83,7 @@
 
        :tools
        ;;ansible
+       biblio
        ;;debugger                      ; FIXME stepping through code, to help you add bugs
        ;;direnv
        ;;docker
@@ -161,7 +162,7 @@
        ;;swift                         ; who asked for emoji variables?
        ;;terra                         ; Earth and Moon in alignment for performance.
        ;;web                           ; the tubes
-       ;;yaml                          ; JSON, but readable
+       yaml                          ; JSON, but readable
 
        :email
        ;;(mu4e +gmail)

ebc6aacbc791e3c4946575becebf6ae94d72150d
hieutkt <hieunguyen31371@gmail.com>
2020-08-19 22:36:15 +0700

    The cursor position in org-roam-capture

--- config.el
+++ config.el
@@ -248,9 +248,9 @@
   :config
   (setq org-roam-capture-templates
         '(("d" "default" plain (function org-roam-capture--get-point)
-           "%?"
+           ""
            :file-name "%<%Y%m%d%H%M%S>-${slug}"
-           :head "#+title: ${title}\n#+roam_tags:\n#+roam_alias:\n"
+           :head "#+title: ${title}\n#+roam_tags: %?\n#+roam_alias: \n"
            :unnarrowed t))
         org-roam-capture-ref-templates
         '(("r" "ref" plain (function org-roam-capture--get-point)

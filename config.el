e976aeeeab0b6951faaea98660f3fc921fb2d79b
hieutkt <hieunguyen31371@gmail.com>
2020-08-29 13:36:49 +0700

    Modify treemacs icons with colors

--- config.el
+++ config.el
@@ -34,7 +34,8 @@
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-gruvbox)
+(setq doom-theme 'doom-gruvbox
+      doom-themes-treemacs-theme 'doom-colors)
 
 ;; Start Doom fullscreen
 (add-to-list 'initial-frame-alist '(fullscreen . maximized))

cf80f8435b5c1bfd42ac9a21b9d1d1c46bb1150c
hieutkt <hieunguyen31371@gmail.com>
2021-02-10 23:58:55 +0700

    Try too keep org-ellipsis' appearance consistent

--- config.el
+++ config.el
@@ -141,7 +141,8 @@
         ;; Cache the preview images elsewhere
         org-preview-latex-image-directory "~/.cache/ltximg/"
         org-highlight-latex-and-related nil
-        org-ellipsis (all-the-icons-material "keyboard_arrow_down"))
+        org-ellipsis ""
+        org-cycle-separator-lines -1)
   ;; Setup custom links
   (+org-init-custom-links-h))
 

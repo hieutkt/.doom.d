c0728cf94e6de5220265b04d5ebbf00db5b7a896
hieutkt <hieunguyen31371@gmail.com>
2021-02-24 16:01:26 +0700

    `pdf-tools` can be compiled in Windows now

--- init.el
+++ init.el
@@ -95,7 +95,7 @@
        (magit (:if IS-LINUX +forge))   ; a git porcelain for Emacs
        ;;make                          ; run make tasks from Emacs
        ;;pass                          ; password manager for nerds
-       (:if IS-LINUX pdf)                             ; pdf enhancements
+       pdf                             ; pdf enhancements
        ;;prodigy                       ; FIXME managing external services & code builders
        ;;rgb                           ; creating color strings
        ;;taskrunner                    ; taskrunner for all your projects

cbc2455fb5e5de2ce16362d01bb706cd26e60813
hieutkt <hieunguyen31371@gmail.com>
2021-05-08 19:26:52 +0700

    Simplify org-preview-latex scaling to 1.1

--- config.el
+++ config.el
@@ -188,7 +188,7 @@
         ;; Make latex preview with "C-c C-x C-l" slightly bigger
         org-format-latex-options
         (plist-put org-format-latex-options
-                   :scale (if IS-WINDOWS 1.1 1.8))
+                   :scale 1.1)
         ;; Cache the preview images elsewhere
         org-preview-latex-image-directory "~/.cache/ltximg/"
         org-highlight-latex-and-related nil
